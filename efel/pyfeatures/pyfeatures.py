ccf26bd02a8c43613152ffd4e166276df48d5b52
Daniel Fernández <daferoes@gmail.com>
2026-08-20 10:55:40 +0200

    Add Open Brain Institute copyright notices

--- efel/pyfeatures/pyfeatures.py
+++ efel/pyfeatures/pyfeatures.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 """
 Copyright (c) 2015, Blue Brain Project/EPFL
+Copyright (c) 2025-2026 Open Brain Institute
 All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
